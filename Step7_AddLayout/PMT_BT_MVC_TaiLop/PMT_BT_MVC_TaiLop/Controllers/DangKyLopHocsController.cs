1dad6be9a75a356f3c84fb8971f0f4da772e344b
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-20 13:02:54 +0700

    PMT_AddLayout

---
+++ Step7_AddLayout/PMT_BT_MVC_TaiLop/PMT_BT_MVC_TaiLop/Controllers/DangKyLopHocsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PMT_Baitap_MVCTaiLop.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DangKyLopHocsController : Controller
+    {
+        private readonly AppDbContext _db;
+        public DangKyLopHocsController(AppDbContext db) => _db = db;
+
+        public IActionResult Index() => View(_db.dangKyLopHocs.Include(d => d.SinhVien).Include(d => d.LopHocPhan).ToList());
+
+        public IActionResult Create()
+        {
+            ViewBag.SinhVienId = new SelectList(_db.sinhViens, "SinhVienId", "tenSinhVien");
+            ViewBag.LopHocPhanId = new SelectList(_db.lopHocPhans, "LopHocPhanId", "tenLop");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DangKyLopHoc dk)
+        {
+            dk.ngaydk = DateTime.Now; 
+            _db.dangKyLopHocs.Add(dk);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var dk = _db.dangKyLopHocs.Find(id);
+            ViewBag.SinhVienId = new SelectList(_db.sinhViens, "SinhVienId", "tenSinhVien", dk.SinhVienId);
+            ViewBag.LopHocPhanId = new SelectList(_db.lopHocPhans, "LopHocPhanId", "tenLop", dk.LopHocPhanId);
+            return View(dk);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DangKyLopHoc dk)
+        {
+            _db.dangKyLopHocs.Update(dk);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var dk = _db.dangKyLopHocs.Find(id);
+            if (dk != null) { _db.dangKyLopHocs.Remove(dk); _db.SaveChanges(); }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Step7_AddLayout/PMT_BT_MVC_TaiLop/PMT_BT_MVC_TaiLop/Controllers/LopHocPhansController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PMT_Baitap_MVCTaiLop.Models;
+
+namespace PMT_Baitap_MVCTaiLop.Controllers
+{
+    public class LopHocPhansController : Controller
+    {
+        private readonly AppDbContext _db;
+        public LopHocPhansController(AppDbContext db) => _db = db;
+
+        public IActionResult Index() => View(_db.lopHocPhans.Include(l => l.GiaoVien).Include(l => l.KhoaHoc).ToList());
+
+        public IActionResult Create()
+        {
+            ViewBag.GiaoVienId = new SelectList(_db.giaoViens, "GiaoVienId", "tenGV");
+            ViewBag.KhoaHocId = new SelectList(_db.khoaHocs, "KhoaHocId", "tenKH");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(LopHocPhan lhp)
+        {
+            _db.lopHocPhans.Add(lhp);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var lhp = _db.lopHocPhans.Find(id);
+            ViewBag.GiaoVienId = new SelectList(_db.giaoViens, "GiaoVienId", "tenGV", lhp.GiaoVienId);
+            ViewBag.KhoaHocId = new SelectList(_db.khoaHocs, "KhoaHocId", "tenKH", lhp.KhoaHocId);
+            return View(lhp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(LopHocPhan lhp)
+        {
+            _db.lopHocPhans.Update(lhp);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var lhp = _db.lopHocPhans.Find(id);
+            if (lhp != null) { _db.lopHocPhans.Remove(lhp); _db.SaveChanges(); }
+            return RedirectToAction("Index");
+        }
+    }
+}
