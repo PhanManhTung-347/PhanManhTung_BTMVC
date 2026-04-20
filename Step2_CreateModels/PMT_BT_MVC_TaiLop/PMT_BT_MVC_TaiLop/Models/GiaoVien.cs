e78e555d040455cb9fae6bacdcf7495d8e0ee8df
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-20 12:36:32 +0700

    PMT_CreateModels

---
+++ Step2_CreateModels/PMT_BT_MVC_TaiLop/PMT_BT_MVC_TaiLop/Models/GiaoVien.cs
@@ -0,0 +1,9 @@
+﻿namespace PMT_Baitap_MVCTaiLop.Models
+{
+    public class GiaoVien
+    {
+        public int GiaoVienId { get; set; }
+        public string tenGV { get; set; }
+        public ICollection<LopHocPhan> lopHocPhans { get; set; }
+    }
+}
