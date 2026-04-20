cf098a3e785274d4fffe8fb24aaf468793a1d399
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-20 12:42:16 +0700

    PMT_CreateDbContextAndConnectionString

---
+++ Step3_CreateDbContextAndConnectionString/PMT_BT_MVC_TaiLop/PMT_BT_MVC_TaiLop/Models/KhoaHoc.cs
@@ -0,0 +1,9 @@
+﻿namespace PMT_Baitap_MVCTaiLop.Models
+{
+    public class KhoaHoc
+    {
+        public int KhoaHocId { get; set; }
+        public string tenKH { get; set; }  
+        public ICollection<LopHocPhan> lopHocPhans { get; set; }
+    }
+}
