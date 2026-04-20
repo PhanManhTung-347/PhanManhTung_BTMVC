a9f9d1d377be31cb2226a469d589096b285cbc9c
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-20 12:51:30 +0700

    PMT_CreateControllers

---
+++ Step5_CreateControllers/PMT_BT_MVC_TaiLop/PMT_BT_MVC_TaiLop/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMT_Baitap_MVCTaiLop.Models;
+
+namespace PMT_Baitap_MVCTaiLop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<KhoaHoc> khoaHocs { get; set; }
+        public DbSet<GiaoVien> giaoViens { get; set; }
+        public DbSet<LopHocPhan> lopHocPhans { get; set; }
+
+        public DbSet<SinhVien> sinhViens { get; set; }
+        public DbSet<DangKyLopHoc> dangKyLopHocs { get; set; }
+    }
+}
---
+++ Step5_CreateControllers/PMT_BT_MVC_TaiLop/PMT_BT_MVC_TaiLop/Models/DangKyLopHoc.cs
@@ -0,0 +1,12 @@
+﻿namespace PMT_Baitap_MVCTaiLop.Models
+{
+    public class DangKyLopHoc
+    {
+        public int DangKyLopHocId { get; set; }
+        public DateTime ngaydk { get; set; }
+        public int SinhVienId { get; set; }
+        public SinhVien SinhVien { get; set; }
+        public int LopHocPhanId { get; set; }
+        public LopHocPhan LopHocPhan { get; set; }
+    }
+}
